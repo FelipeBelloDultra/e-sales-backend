4efb43c6d95a8136f5b92f58cd13b27a4e8a008a
FelipeBelloDultra <felipe_bello_dultra@hotmail.com>
2020-09-07 11:46:18 -0300

    feat: error handling

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,10 +1,35 @@
-import express from 'express';
+/* eslint-disable no-console */
+
+import 'reflect-metadata';
+
+import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
+import 'express-async-errors';
+
+import AppError from '@shared/errors/AppError';
 
 import '@shared/infra/typeorm';
 
 const app = express();
 
-app.get('/', (request, response) => response.send());
+app.use(cors());
+app.use(express.json());
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  console.log(err);
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error.',
+  });
+});
 
 app.listen(3333, () => {
   console.log('🚀 Server running!');
