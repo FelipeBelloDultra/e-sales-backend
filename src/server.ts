47be64265dd3ecf7dd46cc9fc50ce6825763561a
FelipeBelloDultra <felipe_bello_dultra@hotmail.com>
2020-09-07 11:18:46 -0300

    feat: connection with mongodb and typeorm config

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,11 @@
 import express from 'express';
 
+import './shared/infra/typeorm';
+
 const app = express();
 
 app.get('/', (request, response) => response.send());
 
-app.listen(3333);
+app.listen(3333, () => {
+  console.log('🚀 Server running!');
+});
