7aaf668961898ec35ab50bd9e527072571ddfaba
FelipeBelloDultra <felipe_bello_dultra@hotmail.com>
2020-09-07 11:51:57 -0300

    feat: init routes

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -7,6 +7,7 @@ import cors from 'cors';
 import 'express-async-errors';
 
 import AppError from '@shared/errors/AppError';
+import routes from './routes';
 
 import '@shared/infra/typeorm';
 
@@ -14,6 +15,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(routes);
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   if (err instanceof AppError) {
