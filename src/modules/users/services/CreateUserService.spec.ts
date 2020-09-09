cef8de6e7abd357b64269f20cf15de7cd022ca32
FelipeBelloDultra <felipe_bello_dultra@hotmail.com>
2020-09-09 16:47:13 -0300

    dep: fakerjs to fake data for test

--- src/modules/users/services/CreateUserService.spec.ts
+++ src/modules/users/services/CreateUserService.spec.ts
@@ -1,4 +1,5 @@
 import AppError from '@shared/errors/AppError';
+import faker from 'faker';
 
 import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
 import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
@@ -18,31 +19,31 @@ describe('CreateUser', () => {
 
   it('should be able to create a new user', async () => {
     const user = await createUser.execute({
-      name: 'John Doe',
-      email: 'john@doe.com',
-      password: 'john123',
-      whatsapp: '16999999999',
+      name: faker.name.findName(),
+      email: faker.internet.email(),
+      password: faker.random.word(),
+      whatsapp: faker.phone.phoneNumber(),
     });
 
     expect(user).toHaveProperty('id');
   });
 
   it('should not be able to create a new user if the email has already been used', async () => {
-    const email = 'john@doe.com';
+    const email = faker.internet.email();
 
     await createUser.execute({
-      name: 'John Doe',
+      name: faker.name.findName(),
       email,
-      password: 'john123',
-      whatsapp: '16999999999',
+      password: faker.random.word(),
+      whatsapp: faker.phone.phoneNumber(),
     });
 
     await expect(
       createUser.execute({
-        name: 'John Doe 2',
+        name: faker.name.findName(),
         email,
-        password: 'john123',
-        whatsapp: '16999999999',
+        password: faker.random.words(),
+        whatsapp: faker.phone.phoneNumber(),
       }),
     ).rejects.toBeInstanceOf(AppError);
   });
