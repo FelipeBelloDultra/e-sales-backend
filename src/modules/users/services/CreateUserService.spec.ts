bc1b753942f3c8aa6762a60df40c5048d7d56d02
FelipeBelloDultra <felipe_bello_dultra@hotmail.com>
2020-09-08 21:15:22 -0300

    test: check if this email has already registered

--- src/modules/users/services/CreateUserService.spec.ts
+++ src/modules/users/services/CreateUserService.spec.ts
@@ -1,3 +1,5 @@
+import AppError from '@shared/errors/AppError';
+
 import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
 import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
 import CreateUserService from './CreateUserService';
@@ -16,12 +18,32 @@ describe('CreateUser', () => {
 
   it('should be able to create a new user', async () => {
     const user = await createUser.execute({
-      email: 'john@doe.com',
       name: 'John Doe',
+      email: 'john@doe.com',
       password: 'john123',
       whatsapp: '16999999999',
     });
 
     expect(user).toHaveProperty('id');
+  });
+
+  it('should not be able to create a new user if the email has already been used', async () => {
+    const email = 'john@doe.com';
+
+    await createUser.execute({
+      name: 'John Doe',
+      email,
+      password: 'john123',
+      whatsapp: '16999999999',
+    });
+
+    await expect(
+      createUser.execute({
+        name: 'John Doe 2',
+        email,
+        password: 'john123',
+        whatsapp: '16999999999',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
   });
 });
