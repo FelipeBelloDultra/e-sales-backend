00d5eb502990a6cd5bec2e1d2a3a8b63cdf62f64
FelipeBelloDultra <felipe_bello_dultra@hotmail.com>
2020-09-10 08:07:48 -0300

    test: verify if e-mail is wrong in auth test

--- src/modules/users/services/AuthenticateUserService.spec.ts
+++ src/modules/users/services/AuthenticateUserService.spec.ts
@@ -1,5 +1,6 @@
 import faker from 'faker';
 
+import AppError from '@shared/errors/AppError';
 import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
 import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
 import AuthenticateUserService from './AuthenticateUserService';
@@ -37,5 +38,43 @@ describe('AuthenticateUser', () => {
 
     expect(response).toHaveProperty('token');
     expect(response.user).toEqual(user);
+  });
+
+  it('should not be able to authenticate the user if the e-mail is wrong', async () => {
+    const email = faker.internet.email();
+    const password = faker.random.word();
+
+    await fakeUsersRepository.create({
+      name: faker.name.findName(),
+      email,
+      password,
+      whatsapp: faker.phone.phoneNumber(),
+    });
+
+    await expect(
+      authenticateUser.execute({
+        email: faker.internet.email(),
+        password,
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able to authenticate the user if the password is wrong', async () => {
+    const email = faker.internet.email();
+    const password = faker.random.word();
+
+    await fakeUsersRepository.create({
+      name: faker.name.findName(),
+      email,
+      password,
+      whatsapp: faker.phone.phoneNumber(),
+    });
+
+    await expect(
+      authenticateUser.execute({
+        email,
+        password: faker.random.word(),
+      }),
+    ).rejects.toBeInstanceOf(AppError);
   });
 });
