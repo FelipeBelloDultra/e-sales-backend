aa4e2f6b21e15694c5a2e0df375a033eb9aab59d
FelipeBelloDultra <felipe_bello_dultra@hotmail.com>
2020-09-11 18:44:00 -0300

    fix: store route is private

--- src/modules/stores/infra/http/routes/stores.routes.ts
+++ src/modules/stores/infra/http/routes/stores.routes.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 
+import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import StoresController from '../controllers/StoresController';
 
 const storeRouter = Router();
 
 const storesController = new StoresController();
+
+storeRouter.use(ensureAuthenticated);
 
 storeRouter.get('/', storesController.create);
 
